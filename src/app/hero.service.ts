3e12c90ba0595ab990c1704077cb123d6722c14f
zhaoweilong <zhaoweilong0220@163.com>
2019-08-18 23:12:03 +0800

    添加首页顶级英雄

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,11 +1,22 @@
 import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {MessageService} from './message.service';
+import {Observable} from 'rxjs';
+import {Hero} from './hero';
+import {tap} from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
-  constructor() { }
-
-
+  private getAllHeroesUrl = 'http://localhost:8080/hero/getAll';
+  constructor(private http: HttpClient, private messageService: MessageService) { }
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.getAllHeroesUrl);
+  }
 }
